32dd1b73947fd8eafbc406efb557923c0532021a
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2019-07-14 22:18:00 +0100

    new main page

--- sketch.js
+++ sketch.js
@@ -1,2 +1,35 @@
-let size = getComputedStyle(document.body).getPropertyValue("--grid_item_width");
-console.log(size);
+// Set background to canvas
+let sky_color = "rgb(13, 13, 13)";
+let cnv = document.createElement('canvas');
+let ctx = cnv.getContext('2d');
+
+// Use this to set canvas background color
+cnv.setAttribute('width', window.innerWidth);
+cnv.setAttribute('height', window.innerHeight);
+ctx.fillStyle = sky_color;
+ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+makeStars(density=0.0001);
+document.body.style.background = 'url(' + cnv.toDataURL() + ')';
+
+function makeStars(density=0.75, min_size=1, max_size=3) {
+  for( let x = 0; x < cnv.width; x++ ){
+      for( let y = 0; y < cnv.height; y++ ){
+          if( Math.random() < density) {
+            ctx.fillStyle = 'hsl(0, 0%, ' + ( 100 - ( Math.random() * 15 ) ) + '%)';
+            let r = randomExponential(gamma=0.8);
+            let star_size = (max_size - min_size) * r + min_size
+            ctx.beginPath();
+            ctx.ellipse(x, y, star_size, star_size, 0, 0, 2 * Math.PI);
+            ctx.fill();
+          }
+      }
+  }
+}
+
+function randomExponential(gamma=1) {
+  	/*
+    Get random number from negative exponential distribution
+    */
+  return (1 - Math.E**(-gamma*Math.random()))
+}
